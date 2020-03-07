453eca03f68aa8ebe6d81200f33f586e1185289c
gao525280067 <525280067@qq.com>
2020-03-07 18:40:19 +0800

    config: 各个配置初始化

---
+++ .fatherrc.js
@@ -0,0 +1,21 @@
+export default {
+  // cssModules: true, // 默认是 .module.css 走 css modules，.css 不走 css modules。配置 cssModules 为 true 后，全部 css 文件都走 css modules。（less 文件同理）
+  extractCSS: true,
+  esm: 'babel',
+  cjs: 'babel',
+  umd: {
+    name: 'gf-ui',
+    sourcemap: true,
+    globals: {
+      react: 'React'
+    },
+  },
+  extraBabelPlugins: [
+    //['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
+  entry: 'src/index.ts',
+  lessInBabelMode: true,
+  doc: {
+    base: '/'
+  }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# gf-ui
+
+一个基于 React 开发的组件库，包含基础组件、业务组件、区块、hooks、utils 等
+npm i gf-react-ui -D
+
+[
+'feat', // 新功能（feature）
+'improve', // 改进功能
+'fix', // 修补 bug
+'docs', // 文档（documentation）
+'style', // 格式（不影响代码运行的变动）
+'refactor', // 重构（即不是新增功能，也不是修改 bug 的代码变动）
+'test', // 增加测试
+'revert', // 回滚
+'config', // 构建过程或辅助工具的变动
+'chore', // 其他改动
+],
